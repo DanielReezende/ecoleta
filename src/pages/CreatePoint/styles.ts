ad763707b87536286c751ff662813ded5d9367bc
Daniel Rezende <daniel.zende@gmail.com>
2020-11-26 21:55:46 -0300

    Adicionado o style do map e do button de submit

--- src/pages/CreatePoint/styles.ts
+++ src/pages/CreatePoint/styles.ts
@@ -16,8 +16,34 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
 
+    .leaflet-container {
+      width: 100%;
+      height: 350px;
+      border-radius: 8px;
+      margin-bottom: 24px;
+    }
+
     h1 {
       font-size: 36px;
+    }
+
+    button {
+      width: 260px;
+      height: 56px;
+      background: var(--primary-color);
+      border-radius: 8px;
+      color: #FFF;
+      font-weight: bold;
+      font-size: 16px;
+      border: 0;
+      align-self: flex-end;
+      margin-top: 40px;
+      transition: background-color 0.2s;
+      cursor: pointer;
+
+      &:hover{
+        background: #2FB86E;
+      }
     }
   }
 `;
@@ -131,7 +157,7 @@ export const ItemsGroup = styled.ul`
 
       display: flex;
       align-items: center;
-      color: var(--title-color)
+      color: var(--title-color);
     }
 
     &.selected{
